a2fb3695132d79d1774900c69a562ead0d86efc0
mateomolinari <mateomolinari33@gmail.com>
2022-11-11 16:19:06 -0300

    /discord rerout

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/discord',
+        destination: 'https://discord.gg/mHRhER8C5N', // Matched parameters can be used in the destination
+        permanent: true,
+      },
+    ]
+  }
 }
 
 module.exports = nextConfig
