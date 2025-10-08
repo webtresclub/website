4e9e6d41b4623ace52d58b65c85ac97345dcea0d
AlanDooz <1010505+alandooz@users.noreply.github.com>
2025-10-08 19:50:44 -0300

    Remove redirects from next.config.js
    Removed async redirects configuration from nextConfig.

--- next.config.js
+++ next.config.js
@@ -2,20 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  async redirects() {
-    return [
-      {
-        source: '/qr',
-        destination: '/',
-        permanent: false,
-      },
-      {
-        source: '/root',
-        destination: '/',
-        permanent: false,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
